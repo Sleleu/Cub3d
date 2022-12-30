33dd1618f01ff2b94a816a4fd41585019bc501a8
RolandVrignon <roland.vrignon@gmail.com>
2022-12-30 19:36:09 +0100

    minor changes

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -3,38 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sleleu <sleleu@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rvrignon <rvrignon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 20:52:13 by sleleu            #+#    #+#             */
-/*   Updated: 2022/11/26 17:11:40 by sleleu           ###   ########.fr       */
+/*   Updated: 2022/12/30 19:35:24 by rvrignon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft.h"
 
-char    *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-        int             size;
-        int             i;
-        char    *str;
+	int		size;
+	int		i;
+	char	*str;
 
-        i = 0;
-        size = (ft_strlen(s1) + ft_strlen(s2));
-        str = malloc(sizeof(char) * size + 1);
-        str[size] = '\0';
-        if (str == NULL)
-                return (NULL);
-        while (s1 && *s1)
-        {
-                str[i] = *s1;
-                i++;
-                s1++;
-        }
-        while (s2 && *s2)
-        {
-                str[i] = *s2;
-                i++;
-                s2++;
-        }
-        return (str);
+	i = 0;
+	size = (ft_strlen(s1) + ft_strlen(s2));
+	str = malloc(sizeof(char) * size + 1);
+	str[size] = '\0';
+	if (str == NULL)
+		return (NULL);
+	while (s1 && *s1)
+	{
+		str[i] = *s1;
+		i++;
+		s1++;
+	}
+	while (s2 && *s2)
+	{
+		str[i] = *s2;
+		i++;
+		s2++;
+	}
+	return (str);
 }
